5b8818ede1268be833928e2cb2ec0bc8fed0d4ad
BenjaminJaume <benjamin.jaume@gmail.com>
2019-05-09 20:16:42 +1000

    Update

--- script.js
+++ script.js
@@ -1,89 +1,63 @@
-var button = document.getElementById("enter");
-var input = document.getElementById("userInput");
-var ul = document.querySelector("ul")
-var deleteBtns = document.getElementsByClassName("delete");
-var items = ul.getElementsByTagName("li");
-
-
+let button = document.getElementById('enter');
+let input = document.getElementById('userInput');
+let ul = document.querySelector('ul');
+let deleteBtns = document.getElementsByClassName('delete');
+let items = ul.getElementsByTagName('li');
 
 //add event listener to first 6 btns in HTML file
-for(var i = 0; i < deleteBtns.length; i++){
-	deleteBtns[i].addEventListener("click", removeParent, false);
+for (let i = 0; i < deleteBtns.length; i++) {
+  deleteBtns[i].addEventListener('click', removeParent, false);
 }
-
 
 //from StackOverflow:
 function removeParent(evt) {
-  evt.target.removeEventListener("click", removeParent, false);
+  evt.target.removeEventListener('click', removeParent, false);
   evt.target.parentNode.remove();
 }
 
 //click on a list item and it strikethroughs the text
-function getEventTarget(e){
-	e = e || window.event;
-	return e.target || e.srcElement;
+function getEventTarget(e) {
+  e = e || window.event;
+  return e.target || e.srcElement;
 }
 
-
-ul.onclick = function(event){
-	var target = getEventTarget(event);
-	target.classList.toggle("done");
-}
-
+ul.onclick = function(event) {
+  let target = getEventTarget(event);
+  target.classList.toggle('done');
+};
 
 //adding new items:
 
-function inputLength(){
-	return input.value.length;
+function inputLength() {
+  return input.value.length;
 }
 
 function createListElement() {
-	var btn = document.createElement("button");
-	btn.innerHTML = "Delete";
-	btn.onclick = removeParent;
+  let btn = document.createElement('button');
+  btn.innerHTML = 'Delete';
+  btn.onclick = removeParent;
 
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	li.innerHTML = li.innerHTML + " ";
-	li.appendChild(btn);
+  let li = document.createElement('li');
+  li.appendChild(document.createTextNode(input.value));
+  li.innerHTML = li.innerHTML + ' ';
+  li.appendChild(btn);
 
-	ul.appendChild(li);
-	input.value="";
+  ul.appendChild(li);
+  input.value = '';
 }
 
-
-function addToListAfterClick(){
-	if(inputLength() > 0){
-			createListElement();
-		}
+function addToListAfterClick() {
+  if (inputLength() > 0) {
+    createListElement();
+  }
 }
 
-function addToListAfterKeypress(event){
-	if(inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
+function addToListAfterKeypress(event) {
+  if (inputLength() > 0 && event.keyCode === 13) {
+    createListElement();
+  }
 }
 
+button.addEventListener('click', addToListAfterClick);
 
-button.addEventListener("click", addToListAfterClick);
-
-input.addEventListener("keypress", addToListAfterKeypress);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+input.addEventListener('keypress', addToListAfterKeypress);
